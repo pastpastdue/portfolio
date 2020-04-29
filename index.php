7cf9f72eda52625f03d28bd21d8377a7a27d83cb
Eric Croskey <eric@artefactgroup.com>
2020-04-29 15:24:59 -0700

    Update orderby args to sort by most recent.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
  * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
  *
@@ -19,10 +19,10 @@
 
 	$featuredargs = array(
 		'category_name' => 'featured',
-		'orderby' => 'title',
-		'order' => 'ASC'
+		'orderby' => 'date',
+		'order' => 'DESC'
 	);
-	
+
 	$featured = new WP_Query($featuredargs);
 
 ?>
@@ -32,18 +32,18 @@
 					<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/name' ) ); ?>
 					<?php // Starkers_Utilities::get_template_parts( array( 'parts/shared/foursquare' ) ); ?>
 					<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/lastfm' ) ); ?>
-					<?php // Starkers_Utilities::get_template_parts( array( 'parts/shared/search' ) ); ?> 		  
+					<?php // Starkers_Utilities::get_template_parts( array( 'parts/shared/search' ) ); ?>
 <?php while ($featured->have_posts()) : $featured->the_post(); ?>
-					<li class="projectPost"> 
-						<a href="<?php the_permalink(); ?>" title="<?php if (in_category('redact')) {?>█████████████<?php } else { ?><?php the_title(); } ?>"> 
+					<li class="projectPost">
+						<a href="<?php the_permalink(); ?>" title="<?php if (in_category('redact')) {?>█████████████<?php } else { ?><?php the_title(); } ?>">
 							<div class="projectThumb">
 								<div>
 								<?php the_post_thumbnail(); ?>
 								</div>
 							</div>
-							<div class="projectDetails"> 
+							<div class="projectDetails">
 								<h2><?php if (in_category('redact')) {?><span class="redact">█████████████</span><?php } else { ?><?php the_title(); } ?></h2>
-								<h3> 
+								<h3>
 								<?php
 								$posttags = get_the_tags();
 								$count=0;
@@ -57,8 +57,8 @@
 								}
 								?>
 								</h3>
-							</div> 
-						</a> 
+							</div>
+						</a>
 					</li>
 <?php endwhile; ?>
 				</ul>
