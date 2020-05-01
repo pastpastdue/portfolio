d43cde10eb8ca19a62059ea529e48e9a9fac8293
Eric Croskey <eric@artefactgroup.com>
2020-04-30 18:33:17 -0700

    Fix anchor hierarcy

--- parts/shared/confidential.php
+++ parts/shared/confidential.php
@@ -1,7 +1,7 @@
 					<li class="post dataPost">
 						<a href="/" title="Back home.">
-						<h1>CONFIDENTIAL.<br/>DO NOT SHARE.</h1>
-						<p class="orange-divider">---------------------------</p>
-						<p class="summary">The projects showcased here are highly confidential. All work is shown for hiring purposes only. <b>Please <a href="mailto:me@ericcroskey.com?Subject=Your%20Portfolio" title="Email me">inquire</a> for more detail.</b></p>
+							<h1>CONFIDENTIAL.<br/>DO NOT SHARE.</h1>
+							<p class="orange-divider">---------------------------</p>
 						</a>
+						<p class="summary">The projects showcased here are highly confidential. All work is shown for hiring purposes only. <b>Please <a href="mailto:me@ericcroskey.com?Subject=Your%20Portfolio" title="Email me">inquire</a> for more detail.</b></p>
 					</li>
