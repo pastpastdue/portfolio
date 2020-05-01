d6510469106bbdbb6530ca64be8609a12cf50946
Eric Croskey <eric@artefactgroup.com>
2020-04-30 18:31:53 -0700

    Update langauge.

--- parts/shared/confidential.php
+++ parts/shared/confidential.php
@@ -2,6 +2,6 @@
 						<a href="/" title="Back home.">
 						<h1>CONFIDENTIAL.<br/>DO NOT SHARE.</h1>
 						<p class="orange-divider">---------------------------</p>
-						<p class="summary">The projects showcased here are highly confidential. All work is shown for hiring purposes only. <b>Please do not share.</b></p>
+						<p class="summary">The projects showcased here are highly confidential. All work is shown for hiring purposes only. <b>Please <a href="mailto:me@ericcroskey.com?Subject=Your%20Portfolio" title="Email me">inquire</a> for more detail.</b></p>
 						</a>
 					</li>
