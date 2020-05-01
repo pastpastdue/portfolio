077e02816a2e1b13ae8142f1e60967391ad7aba4
Eric Croskey <eric@artefactgroup.com>
2020-04-30 18:28:37 -0700

    Confidentiality banner.

---
+++ parts/shared/confidential.php
@@ -0,0 +1,7 @@
+					<li class="post dataPost">
+						<a href="/" title="Back home.">
+						<h1>CONFIDENTIAL.<br/>DO NOT SHARE.</h1>
+						<p class="orange-divider">---------------------------</p>
+						<p class="summary">The projects showcased here are highly confidential. All work is shown for hiring purposes only. <b>Please do not share.</b></p>
+						</a>
+					</li>
--- single.php
+++ single.php
@@ -13,19 +13,7 @@
 <?php get_sidebar(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<ul class="projectList fixedDiv">
-			<?php
-			$input = array( 'parts/shared/name.php',
-				// 'parts/shared/foursquare.php',
-				'parts/shared/lastfm.php',
-				//'parts/shared/search.php',
-				//'parts/shared/foursquare.php',
-				'parts/shared/lastfm.php',
-				//'parts/shared/foursquare.php',
-				'parts/shared/lastfm.php' );
-				  $rand_keys = array_rand($input, 2);
-
-			?>
-			<?php include($input[$rand_keys[0]]); ?>
+			<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/confidential' ) ); ?>
 		</ul>
 
 
