2f4dc566a428ee1af32ecee388d8b95c13d1bb5f
Eric Croskey <eric@artefactgroup.com>
2020-07-09 19:41:02 -0700

    Meta updates

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -6,15 +6,35 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( ' ' ); if (is_home()) { } else { ?>. <?php } ?></title>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<meta name="title" content="Eric Croskey. Design and data.">
+		<meta name="description" content="Interdisciplinary designer who works at the intersection of design, code and research.">
+		<meta name="keywords" content="eric,croskey,eric croskey,designer,college for creative studies,ccs,artefact,seattle,artefact,portfolio">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="The portfolio of Eric Croskey, an interdisciplinary designer from Detroit, MI who now resides in Seattle, WA.">
-		<meta name="keywords" content="eric,croskey,eric croskey,designer,college for creative studies,ccs,artefact,seattle,detroit,bloomfield hills,portfolio">
+
 		<meta name="author" content="Eric Croskey">
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+
+		<!-- Open Graph / Facebook -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://2020.ericcroskey.com/">
+		<meta property="og:title" content="Eric Croskey. Design and data.">
+		<meta property="og:description" content="Interdisciplinary designer who works at the intersection of design, code and research.">
+		<meta property="og:image" content="">
+
+		<!-- Twitter -->
+		<meta property="twitter:card" content="summary_large_image">
+		<meta property="twitter:url" content="https://2020.ericcroskey.com/">
+		<meta property="twitter:title" content="Eric Croskey. Design and data.">
+		<meta property="twitter:description" content="Interdisciplinary designer who works at the intersection of design, code and research.">
+		<meta property="twitter:image" content="">
+
+<!-- Primary Meta Tags -->
+
 
 	</head>
 	<body <?php body_class(); ?>>
