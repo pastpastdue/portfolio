8eebe484a07ef61334686927a00695eb0588be28
Eric Croskey <eric@artefactgroup.com>
2020-07-27 10:01:46 -0700

    Update information page

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -17,7 +17,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="apple-touch-icon" sizes="180x180" href="http://ericcroskey.com/2020/wp-content/themes/portfolio/img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="http://ericcroskey.com/2020/wp-content/themes/portfolio/img/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="http://ericcroskey.com/2020/wp-content/themes/portfolio/img/favicon-16x16.png">
 
 		<!-- Open Graph / Facebook -->
 		<meta property="og:type" content="website">
